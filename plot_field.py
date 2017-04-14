39e3bd7325710673860afae6826da511102fc1b9
Zhen <zhen.lu@kaust.edu.sa>
2017-04-14 15:27:17 +0300

    plot instantaneous field

--- plot_field.py
+++ plot_field.py
@@ -5,7 +5,6 @@ from file_read import csv_read, cm2inch
 #import matplotlib as mpl
 #mpl.use('Agg')
 import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
 
 # variable to be plot
 var = 'T'
@@ -15,7 +14,7 @@ vmax = 2200.0;
 xD_limit=25.0
 rD_limit=5.0
 
-# import data
+# import data average
 field_mean=csv_read('mean_field.csv')
 # convert from 1D to 2D array
 r = sorted(list(set(field_mean['r'])))
@@ -25,6 +24,17 @@ Z = [[0.0 for rD in r ] for zD in z ]
 for i in range(len(field_mean[var])):
     v[z.index(field_mean['z'][i])][r.index(field_mean['r'][i])]=field_mean[var][i]
     Z[z.index(field_mean['z'][i])][r.index(field_mean['r'][i])]=field_mean['Z'][i]
+
+# import data instantaneous
+field_inst=csv_read('inst_field.csv')
+# convert from 1D to 2D array
+ri= sorted(list(set(field_inst['r'])))
+zi= sorted(list(set(field_inst['z'])))
+vi= [[0.0 for rD in ri ] for zD in zi ]
+Zi= [[0.0 for rD in ri ] for zD in zi ]
+for i in range(len(field_inst[var])):
+    vi[zi.index(field_inst['z'][i])][ri.index(field_inst['r'][i])]=field_inst[var][i]
+    Zi[zi.index(field_inst['z'][i])][ri.index(field_inst['r'][i])]=field_inst['Z'][i]
 
 # plot
 # use TEX for interpreter
@@ -62,12 +72,12 @@ ax0 = plt.axes([margin_left/plot_width,
                 2.0*subplot_width/plot_width,
                 subplot_height/plot_height],
                 aspect='equal')
-ax0.contourf(r,z,v,60,
+ax0.contourf(ri,zi,vi,60,
              vmin=vmin,vmax=vmax)
-ax0.contour(r,z,Z,[0.351,],
+ax0.contour(ri,zi,Zi,[0.351,],
             linewidths=2.0)
 # limits
-ax0.set_xlim(0.0,2.0*rD_limit)
+ax0.set_xlim(-rD_limit,rD_limit)
 ax0.set_ylim(0.0,xD_limit)
 # lables
 ax0.set_xlabel('$r/D$',fontsize=ftsize)
@@ -102,14 +112,8 @@ cax = plt.axes([(plot_width-margin_right+clb_left)/plot_width,
                 clb_height/plot_height])
 cbar=plt.colorbar(cf1,cax=cax,ticks=[300,500,700,900,1100,1300,1500,1700,1900,2100])
 
-
-# set margins
-#plt.subplots_adjust(left    =margin_left/plot_width,
-#                    bottom  =margin_bottom/plot_height,
-#                    right   =1.0-margin_right/plot_width,
-#                    top     =1.0-margin_top/plot_height,
-#                    wspace  =space_width/plot_width,
-#                    hspace  =space_height/plot_height)
-
-
-plt.show()
+# save plot
+plt.savefig('field.png',dpt=400)
+plt.savefig('field.pdf')
+plt.savefig('field.eps')
+#plt.show()
