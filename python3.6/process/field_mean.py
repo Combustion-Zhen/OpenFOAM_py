32ab3a817816c26c192c5cff52beaefcd0cd23d6
Zhen <zhen.lu@kaust.edu.sa>
2017-04-24 20:36:28 +0300

    update field_mean.py, with ordered write

--- python3.6/process/field_mean.py
+++ python3.6/process/field_mean.py
@@ -1,6 +1,9 @@
-# Zhen Lu, 11/04/2017
-# load and process flameletFoam results
-# field mean
+"""
+Zhen Lu, 11/04/2017, <albert.lz07@gmail.com>
+modified 24/04/2017, making the r, z ordered
+load and process flameletFoam results
+field mean
+"""
 import glob
 import math
 import csv
@@ -21,7 +24,8 @@ foldername='../{t}/'.format(t=time)
 filename_int_var='Mean'
 filename_int_rms='Prime2Mean'
 # variables
-for filename in glob.glob('{0}*{1}'.format(foldername,filename_int_rms)):
+for filename in glob.glob('{0}*{1}'.format(foldername,
+                                           filename_int_rms)):
     pos = filename.find(filename_int_rms)
     var_names.append(filename[len(foldername):pos])
 # the velocity has three components, remove it at first
@@ -35,27 +39,28 @@ print('Reading coordinates')
 x = comp_read_scalar('{0}ccx'.format(foldername))
 y = comp_read_scalar('{0}ccy'.format(foldername))
 z = comp_read_scalar('{0}ccz'.format(foldername))
-rz=[]
 for i in range(len(z)):
     r = math.sqrt(x[i]**2+y[i]**2)
     #round the coordinates
     field_data['r'].append(float('{0:.3g}'.format(r)))
     field_data['z'].append(float('{0:.3g}'.format(z[i])))
-    rz.append((field_data['r'][i],field_data['z'][i]))
+del x, y, z
 # check the number of r and z points
-print(len(set(field_data['r'])))
-print(len(set(field_data['z'])))
-rz=list(set(rz))
+rd=sorted(list(set(field_data['r'])))
+xd=sorted(list(set(field_data['z'])))
+print(len(rd))
+print(len(xd))
 
 field_mean={}
-for loc in rz:
-    field_mean.update({loc+('count',):0})
-    for var in var_names:
-        field_mean.update({loc+(var,):0.0})
-        field_mean.update({loc+(var+'rms',):0.0})
+for z in xd:
+    for r in rd:
+        field_mean.update({(z,r,'count'):0})
+        for var in var_names:
+            field_mean.update({(z,r,var):0.0})
+            field_mean.update({(z,r,var+'rms'):0.0})
 # It should be 60 for Sandia Flame case with axisymmetric mesh
-for i in range(len(z)):
-    field_mean[(field_data['r'][i],field_data['z'][i],'count')]+=1
+for i,z in enumerate(field_data['z']):
+    field_mean[(z,field_data['r'][i],'count')]+=1
 # read in data
 for var in var_names:
     print('Reading avaraged data: {0}'.format(var))
@@ -66,15 +71,19 @@ for var in var_names:
                                                  var,
                                                  filename_int_rms))
     # sum
-    for i in range(len(z)):
-        field_mean[(field_data['r'][i],field_data['z'][i],var)]+=data_var[i]
-        field_mean[(field_data['r'][i],field_data['z'][i],var+'rms')]+=data_rms[i]
+    for i,z in enumerate(field_data['z']):
+        field_mean[(z,field_data['r'][i],var)]+=data_var[i]
+        field_mean[(z,field_data['r'][i],var+'rms')]+=data_rms[i]
     # average
-    for loc in rz:
-        field_mean[loc+(var,)]      /=float(field_mean[loc+('count',)])
-        field_mean[loc+(var+'rms',)]/=float(field_mean[loc+('count',)])
-        # garuantee it is large or equal to 0
-        field_mean[loc+(var+'rms',)] =math.sqrt(max([field_mean[loc+(var+'rms',)],0.0]))
+    for z in xd:
+        for r in rd:
+            field_mean[(z,r,var)] \
+                /= float(field_mean[(z,r,'count')])
+            field_mean[(z,r,var+'rms')] \
+                /= float(field_mean[(z,r,'count')])
+            # garuantee it is large or equal to 0
+            field_mean[(z,r,var+'rms')] \
+                = math.sqrt(max([field_mean[(z,r,var+'rms')],0.0]))
 
 # write to csv file
 with open('mean_field.csv','w') as csvfile:
@@ -84,9 +93,12 @@ with open('mean_field.csv','w') as csvfile:
         field_names.append(var+'rms')
     writer = csv.DictWriter(csvfile,fieldnames=field_names)
     writer.writeheader()
-    for loc in rz:
-        data_row={'r':'{:e}'.format(loc[0]/D),'z':'{:e}'.format(loc[1]/D)}
-        for var in var_names:
-            data_row.update({var:'{:e}'.format(field_mean[loc+(var,)])})
-            data_row.update({var+'rms':'{:e}'.format(field_mean[loc+(var+'rms',)])})
-        writer.writerow(data_row)
+    for z in xd:
+        for r in rd:
+            data_row={'r':'{:e}'.format(r/D),'z':'{:e}'.format(z/D)}
+            for var in var_names:
+                data_row.update({var:
+                    '{:e}'.format(field_mean[(z,r,var)])})
+                data_row.update({var+'rms':
+                    '{:e}'.format(field_mean[(z,r,var+'rms')])})
+            writer.writerow(data_row)
