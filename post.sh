b4640a23b7111eac7989293b512f2cae3ea8a4bf
Zhen <zhen.lu@kaust.edu.sa>
2017-05-15 14:00:36 +0300

    sampleDict

--- post.sh
+++ post.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 TIME=-latestTime
-SAMPLEDICT="axial xD xnormal"
+SAMPLEDICT="axial xD xnormal inlet"
 ## convert from binary format to ascii
 sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.25/g" \
     -e "s/@DELTAT@/1e-6/g" -e "s/@WRITEINTERVAL@/0.01/g" \
@@ -15,6 +15,7 @@ writeCellCentres $TIME
 ## flameletFoam reconstruction of the fields
 flameletFoamPost $TIME -fields '(CH4 O2 N2 H2O CO2 CO OH H2 NO)'
 ## sample the results
+cp $HOME/$WM_PROJECT/py_scripts/sampleDict/sampleDict* system/
 for i in $SAMPLEDICT
 do
     sample $TIME -dict system/sampleDict_$i
@@ -22,7 +23,8 @@ done
 ## copy the python post-processing scripts
 cd postProcessing
 cp $HOME/$WM_PROJECT/py_scripts/python3.6/file_read.py .
-cp $HOME/$WM_PROJECT/py_scripts/python3.6/process/* .
+cp $HOME/$WM_PROJECT/py_scripts/python3.6/process/radial* .
+cp $HOME/$WM_PROJECT/py_scripts/python3.6/process/field* .
 cp $HOME/$WM_PROJECT/py_scripts/python3.6/plot_single/* .
 ## with python3 alias as py
 python3 radial_mean_digit.py
@@ -32,6 +34,7 @@ python3 field_inst.py
 python3 field_mean.py
 python3 plot_radial_mean.py
 python3 plot_radial_mean_U.py
+python3 plot_radial_mean_U_up.py
 python3 plot_cond.py
 python3 plot_scat.py
 python3 plot_field.py
